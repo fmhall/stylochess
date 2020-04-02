b76b642b3cf0b8890ff4ce2dc1d9461ae879739e
Mason <mason.hall@bloxroute.com>
2020-04-02 00:01:56 -0500

    cli and downloader framework

---
+++ src/chess_stylometry/cli.py
@@ -0,0 +1,87 @@
+import os
+import argparse
+from dataclasses import dataclass
+from typing import Optional
+from datetime import datetime
+
+abs_path = os.path.dirname(os.path.abspath(__file__))
+
+
+@dataclass
+class Arguments:
+    path_to_pgns: str
+    analyze: bool
+    download: bool
+    player_name: Optional[str]
+    pgn_source: Optional[str]
+    start_month: Optional[int]
+    end_month: Optional[int]
+    start_year: Optional[int]
+    end_year: Optional[int]
+
+
+def get_opts() -> argparse.Namespace:
+
+    arg_parser = argparse.ArgumentParser(
+        description="Command line interface for the chess stylometry.",
+        usage="stylochess --help",
+    )
+    arg_parser.add_argument(
+        "--path-to-pgns",
+        help="Path to a folder of pgn files or a single pgn file",
+        type=str,
+    )
+    arg_parser.add_argument(
+        "--analyze", help="Boolean flag to analyze", type=bool
+    )
+    arg_parser.add_argument(
+        "--download", help="Boolean flag to download PGNS", type=bool
+    )
+    arg_parser.add_argument(
+        "--player-name", help="Account name of person you would like to download PGNS from", type=str, default=None
+    )
+    arg_parser.add_argument(
+        "--pgn_source", help="Lichess (L) or Chess.com (C). Default lichess", type=str, default="L"
+    )
+    arg_parser.add_argument(
+        "--start-month", help="Starting month of PGNs to download (1-12)", type=int, default=None
+    )
+    arg_parser.add_argument(
+        "--end-month", help="Ending month of PGNs to download (1-12)", type=int, default=None
+    )
+    arg_parser.add_argument(
+        "--start-year", help="Starting year of PGNs to download (2012-)", type=int, default=None
+    )
+    arg_parser.add_argument(
+        "--end-year", help="Ending month of PGNs to download (2012-)", type=int, default=None
+    )
+    opts, _unknown = arg_parser.parse_known_args()
+    return opts
+
+
+def type_arguments(opts: argparse.Namespace) -> Arguments:
+    if opts.download:
+        assert all([opts.player_name, opts.start_month, opts.end_month, opts.start_year, opts.end_year])
+
+    return Arguments(
+        path_to_pgns=opts.path_to_pgns,
+        analyze=opts.analyze,
+        download=opts.download,
+        player_name=opts.player_name,
+        pgn_source=opts.pgn_source,
+        start_month=opts.start_month,
+        end_month=opts.end_month,
+        start_year=opts.start_year,
+        end_year=opts.end_year
+        )
+
+
+def parse_arguments(args: Arguments):
+    if args.download:
+        cur_year = datetime.today().year
+        assert 1 <= args.start_month <= 12
+        assert 1 <= args.end_month <= 12
+        assert 2012 <= args.start_month <= cur_year
+        assert 2012 <= args.start_month <= cur_year
+        assert args.pgn_source in ['L', 'C']
+        assert args.player_name.isalnum()
---
+++ src/chess_stylometry/main.py
@@ -0,0 +1,15 @@
+from chess_stylometry import cli
+from chess_stylometry import downloader
+
+
+if __name__ == "__main__":
+    opts = cli.get_opts()
+    args = cli.type_arguments(opts)
+    cli.parse_arguments(args)
+
+    if args.download:
+        # Download PGNS
+        pass
+    if args.analyze:
+        # Analyze
+        pass
