9d71cff9f6165d9b549fc7b0d8183ed6ca467887
Mason <mason.hall@bloxroute.com>
2020-06-17 17:40:24 -0500

    added requirement and ability to plot single player time scale

---
+++ src/chess_stylometry/utils/plot_schedule.py
@@ -0,0 +1,27 @@
+from chess_stylometry.utils import time_deltas
+import matplotlib.pyplot as plt
+import datetime
+
+player = "synapz"
+wdir = "../test"
+pgn_name = "games.pgn"
+
+
+def get_plot(wdir, player, pgn_name):
+    player_dts = time_deltas.get_UTC_dates_and_times(wdir, player, pgn_name)
+    hour_to_count = [0 for _ in range(24)]
+    for dt in player_dts:
+        hour = (dt.hour - 6) % 24
+        hour_to_count[hour] += 1
+    return hour_to_count
+
+
+if __name__ == "__main__":
+    player_hour_to_count = get_plot(wdir, player, pgn_name)
+    plt.figure()
+    plt.plot(player_hour_to_count)
+    plt.title(f"Number of games played per hour of the day for {player}")
+    plt.xlabel("Hour of the day (CST)")
+    plt.ylabel("# of games played")
+    plt.tight_layout()
+    plt.show()
